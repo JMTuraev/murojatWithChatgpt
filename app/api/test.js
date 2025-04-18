79a62e1bd93506d59eebd6d6bff7d0c45b1941dc
jafar <jafaralituraev@gmail.com>
2025-04-18 21:03:32 +0500

    api test

--- app/api/test.js
+++ app/api/test.js
@@ -1,7 +1,8 @@
 // pages/api/test.js
+
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    console.log('✅ GET so‘rovi qabul qilindi');
+    console.log('✅ GET so‘rovi qabul qilindi!');
     res.status(200).json({ message: 'Salom dunyo!' });
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
